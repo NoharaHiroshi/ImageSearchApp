14825edb6c79531b157637056bdaeae3eabcef6f
Lands <380788433@qq.com>
2017-09-21 18:31:34 +0800

    测试Angular2

---
+++ manage_app/static/js/my-app/src/app/manage_index.ts
@@ -0,0 +1,6 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { ManageIndexModule } from './manage_index/manage_index.module';
+
+platformBrowserDynamic().bootstrapModule(ManageIndexModule);
---
+++ manage_app/static/js/my-app/src/app/manage_index/manage_index.component.ts
@@ -0,0 +1,17 @@
+declare var $: any;
+
+import { Component, OnInit, OnDestroy, Input  } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+
+
+@Component({
+  selector: 'manage-index-root',
+  templateUrl: '../../tpl/app.html',
+})
+
+export class ManageIndexComponent {
+}
+
+
+
+	  
