4f9eeb0c3d660d1bda57200a09b06fc0f29a2a6b
Lands <380788433@qq.com>
2017-09-26 16:56:18 +0800

    测试Angular启动

--- manage_app/static/js/my-app/config/webpack.dev.js
+++ manage_app/static/js/my-app/config/webpack.dev.js
@@ -3,6 +3,11 @@ var commonConfig = require('./webpack.common.js');
 const helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
+	entry : {
+		'polyfills' : './src/polyfills.ts', // 运行Angular时所需的一些标准js
+		'vendor' : [ './src/vendor.ts', './src/common.js' ], // Angular、Lodash、bootstrap.css......
+		'app' : [ './src/main.ts' ], // 应用代码
+	},
   output   : {
     path      : helpers.root('dist/debug'),
     publicPath: '/',
