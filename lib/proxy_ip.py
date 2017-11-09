5e36774738c8892b5a44133487746b881bf73fd2
Lands <380788433@qq.com>
2017-11-09 18:22:31 +0800

    添加获取代理

--- lib/proxy_ip.py
+++ lib/proxy_ip.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup as bs
 from redis_store.redis_cache import proxy_redis
 
 
+# 获取代理ip
 def get_proxy(page_num=1):
     timeout = 3
     headers = {
@@ -41,6 +42,7 @@ def get_proxy(page_num=1):
     return cleaned_data
 
 
+# 存储代理ip
 def restore_redis_proxy(proxies):
     for index, proxy in enumerate(proxies):
         for _type, ip in proxy.items():
@@ -50,6 +52,19 @@ def restore_redis_proxy(proxies):
                 proxy_redis.hset('https_proxies', ip, False)
 
 
+# 获取代理ip
+def get_redis_proxy(proxy_type):
+    proxies_list = list()
+    _type = proxy_type.split('_')[0]
+    all_proxies = proxy_redis.hgetall('https_proxies')
+    for ip in all_proxies.keys():
+        proxies_list.append({
+            _type: ip
+        })
+    return proxies_list
+
+
+# 验证代理ip
 def validate_proxy_ip(proxy_ips):
     """
     :param proxy_ips: [
@@ -62,38 +77,23 @@ def validate_proxy_ip(proxy_ips):
     ]
     """
     # 本机IP
-    local_ip = '103.233.130.246'
+    local_ip = '127.0.0.1'
     # 有效代理IP池
     proxy_pool = []
     # 超时时间
-    timeout = 10
-    headers = {
-        'Connection': 'keep-alive',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
-                      '(KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-        'Accept-Encoding': 'gzip, deflate, sdch',
-        'Accept-Language': 'zh-CN,zh;q=0.8',
-    }
+    timeout = 3
     for ip in proxy_ips:
         try:
-            response = requests.get('http://ip.chinaz.com/getip.aspx', headers=headers, proxies=ip, timeout=timeout)
+            response = requests.get('http://localhost:8888/lib/validate_proxy_ip', proxies=ip, timeout=timeout)
             result = ujson.loads(response.content)
+            print result
         except Exception as e:
             continue
-        if result['ip'] != local_ip:
-            proxy_pool.append(ip)
     return proxy_pool
 
 
 if __name__ == '__main__':
     # 待清洗代理IP数据池
-    # proxies = get_proxy(page_num=10)
-    # cleaned_proxy = validate_proxy_ip(proxies)
-    # restore_redis_proxy(cleaned_proxy)
-    # for proxy in proxies:
-    #     for ip in proxy.values():
-    #         print ip
-    # validate_proxy_ip([{'https':'111.155.116.200:8123'}])
-    print ujson.dumps({'https': '111.155.116.200:8123'})
+    proxies = get_redis_proxy('https_proxies')
+    print proxies
+    validate_proxy_ip(proxies)
