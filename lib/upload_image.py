64f44780905c7be21c5f0c120c6f595809ebbc3b
Lands <380788433@qq.com>
2017-11-16 14:46:57 +0800

    上传图片

--- lib/upload_image.py
+++ lib/upload_image.py
@@ -1,7 +1,5 @@
 # coding:utf-8
 
-import datetime
-import hashlib
 import os
 from PIL import Image
 from manage_app.config import config
@@ -11,43 +9,43 @@ from model.base import IdGenerator, HashName
 
 
 def save_images(images, image_type='img'):
-    for image in images:
-        _id = IdGenerator.gen()
-        file_name = image.filename
-        original_name = HashName.gen(_id, info="original")
-        preview_name = HashName.gen(_id, info="preview")
-        thumbnail_name = HashName.gen(_id, info="thumbnail")
+    with get_session() as db_session:
+        for image in images:
+            _id = IdGenerator.gen()
+            file_name = image.filename
+            original_name = HashName.gen(_id, info="original")
+            preview_name = HashName.gen(_id, info="preview")
+            thumbnail_name = HashName.gen(_id, info="thumbnail")
 
-        # 背景图片处理方式
-        if image_type == 'img':
-            upload_src = config.IMG_UPLOAD_SRC
-        elif image_type == 'icon':
-            upload_src = config.ICON_UPLOAD_SRC
-        else:
-            upload_src = config.IMG_UPLOAD_SRC
-        # 图像处理
-        im = Image.open(image)
-        preview_im = im.copy()
-        thumb_im = im.copy()
-        # 长宽
-        width, height = im.size
-        # 格式
-        file_format = im.format
-        # 模式
-        mode = im.mode
-        im.save(os.path.join(upload_src, 'original', '.'.join([original_name, file_format.lower()])).replace('\\', '/'))
-        # 预览图（高度固定）
-        preview_height = int(config.THUMBNAIL_IMG_HEIGHT)
-        preview_width = int(width / (float(height) / preview_height))
-        preview_im.thumbnail((preview_width, preview_height))
-        preview_im.save(os.path.join(upload_src, 'preview', '.'.join([preview_name, file_format.lower()])).replace('\\', '/'))
-        # 缩略图（等比例缩放）
-        thumb_height = int(config.PREVIEW_IMG_HEIGHT)
-        thumb_width = int(width / (float(height) / thumb_height))
-        thumb_im.thumbnail((thumb_width, thumb_height))
-        thumb_im.save(os.path.join(upload_src, 'thumbnail', '.'.join([thumbnail_name, file_format.lower()])).replace('\\', '/'))
-        # 数据库存储
-        with get_session() as db_session:
+            # 背景图片处理方式
+            if image_type == 'img':
+                upload_src = config.IMG_UPLOAD_SRC
+            elif image_type == 'icon':
+                upload_src = config.ICON_UPLOAD_SRC
+            else:
+                upload_src = config.IMG_UPLOAD_SRC
+            # 图像处理
+            im = Image.open(image)
+            preview_im = im.copy()
+            thumb_im = im.copy()
+            # 长宽
+            width, height = im.size
+            # 格式
+            file_format = im.format
+            # 模式
+            mode = im.mode
+            im.save(os.path.join(upload_src, 'original', '.'.join([original_name, file_format.lower()])).replace('\\', '/'))
+            # 预览图（高度固定）
+            preview_height = int(config.THUMBNAIL_IMG_HEIGHT)
+            preview_width = int(width / (float(height) / preview_height))
+            preview_im.thumbnail((preview_width, preview_height))
+            preview_im.save(os.path.join(upload_src, 'preview', '.'.join([preview_name, file_format.lower()])).replace('\\', '/'))
+            # 缩略图（等比例缩放）
+            thumb_height = int(config.PREVIEW_IMG_HEIGHT)
+            thumb_width = int(width / (float(height) / thumb_height))
+            thumb_im.thumbnail((thumb_width, thumb_height))
+            thumb_im.save(os.path.join(upload_src, 'thumbnail', '.'.join([thumbnail_name, file_format.lower()])).replace('\\', '/'))
+            # 数据库存储
             img = Img()
             img.id = _id
             img.name = file_name
@@ -59,7 +57,7 @@ def save_images(images, image_type='img'):
             img.mode = mode
             img.height = height
             db_session.add(img)
-    db_session.commit()
+        db_session.commit()
 
 if __name__ == '__main__':
     pass
