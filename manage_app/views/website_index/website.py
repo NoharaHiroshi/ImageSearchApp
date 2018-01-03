834fc316cdd73b3bcfd72a3ae626766c0d5cee4d
Lands <380788433@qq.com>
2018-01-03 17:42:52 +0800

    前端首页切割

--- manage_app/views/website_index/website.py
+++ manage_app/views/website_index/website.py
@@ -9,6 +9,9 @@ from sqlalchemy import or_, func, and_
 
 from model.session import get_session
 from model.manage.user import User
+from model.website.menu import WebsiteMenu
+from model.website.banner import Banner
+from model.website.hot_search import WebsiteHotSearch
 
 from lib.aes_encrypt import AESCipher
 
@@ -25,9 +28,40 @@ def website_manage_index():
         abort(400)
 
 
-@website.route('/website_info', methods=['GET'])
-def get_website_info():
+@website.route('/header', methods=['GET'])
+def get_website_header():
+    result = {
+        'response': 'ok',
+        'banner_list': [],
+        'menu_list': [],
+        'hot_search_list': []
+    }
     try:
-        pass
+        with get_session() as db_session:
+            all_banner = db_session.query(Banner).all()
+            all_hot_search = db_session.query(WebsiteHotSearch).filter(
+                WebsiteHotSearch.status == WebsiteHotSearch.STATUS_NORMAL
+            ).all()
+            all_menu = db_session.query(WebsiteMenu).all()
+            _banner_list = list()
+            _hot_search_list = list()
+            _menu_list = list()
+            for banner in all_banner:
+                banner_dict = banner.to_dict()
+                img_url = banner.get_banner_img(db_session)
+                banner_dict['img_url'] = img_url
+                _banner_list.append(banner_dict)
+            for menu in all_menu:
+                menu_dict = menu.to_dict()
+                _menu_list.append(menu_dict)
+            for hot_search in all_hot_search:
+                hot_search_dict = hot_search.to_dict()
+                _hot_search_list.append(hot_search_dict)
+            result.update({
+                'banner_list': _banner_list,
+                'menu_list': _menu_list,
+                'hot_search_list': _hot_search_list
+            })
+        return jsonify(result)
     except Exception as e:
         print e
