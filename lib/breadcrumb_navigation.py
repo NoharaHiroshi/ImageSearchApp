9ca1d607397fa40b7bde672c1eba1959db581130
Lands <380788433@qq.com>
2018-05-11 18:33:48 +0800

    面包屑导航修改

--- lib/breadcrumb_navigation.py
+++ lib/breadcrumb_navigation.py
@@ -10,46 +10,52 @@ from model.image.image_series import ImageSeries, ImageSeriesCategory, ImageSeri
 def breadcrumb_navigation(url):
     url = ''.join(['#', url])
     nav_list = list()
-    # 链接名称、类型、链接参数
-    # 0：首页， 1：图片系列页， 2：图片系列分类页
-    nav_list.append([u'首页', 0, r'/'])
     url_list = url.split('#/')
-    url_info = url_list[1]
-    url_info_list = url_info.split('/')
-    url_type = url_info_list[0]
-    url_id = url_info_list[1]
-    with get_session() as db_session:
-        if url_type == 'image_detail':
-            image_series = db_session.query(ImageSeries).join(
-                ImageSeriesRel, ImageSeriesRel.image_series_id == ImageSeries.id
-            ).filter(
-                ImageSeriesRel.image_id == url_id
-            ).first()
-            if image_series:
-                image_series_category = db_session.query(ImageSeriesCategory).join(
-                    ImageSeriesCategoryRel, ImageSeriesCategoryRel.category_id == ImageSeriesCategory.id
-                ).filter(
-                    ImageSeriesCategoryRel.series_id == image_series.id
-                ).first()
-                if image_series_category:
-                    nav_list.append([image_series_category.name, 2, str(image_series_category.id)])
-                nav_list.append([image_series.name, 1, str(image_series.id)])
-        elif url_type == 'image_list':
-            image_series = db_session.query(ImageSeries).get(url_id)
-            if image_series:
-                image_series_category = db_session.query(ImageSeriesCategory).join(
-                    ImageSeriesCategoryRel, ImageSeriesCategoryRel.category_id == ImageSeriesCategory.id
-                ).filter(
-                    ImageSeriesCategoryRel.series_id == image_series.id
-                ).first()
-                if image_series_category:
-                    nav_list.append([image_series_category.name, 2, str(image_series_category.id)])
-                nav_list.append([image_series.name, 1, str(image_series.id)])
-        elif url_type == 'image_series_list':
-            image_series_category = db_session.query(ImageSeriesCategory).get(url_id)
-            if image_series_category:
-                nav_list.append([image_series_category.name, 2, str(image_series_category.id)])
-    return nav_list
+    if len(url_list) == 1:
+        return nav_list
+    else:
+        # 链接名称、类型、链接参数
+        # 0：首页， 1：图片系列页， 2：图片系列分类页
+        nav_list.append([u'首页', 0, r'/'])
+        url_info = url_list[1]
+        url_info_list = url_info.split('/')
+        if len(url_info_list) > 1:
+            url_type = url_info_list[0]
+            url_id = url_info_list[1]
+            with get_session() as db_session:
+                if url_type == 'image_detail':
+                    image_series = db_session.query(ImageSeries).join(
+                        ImageSeriesRel, ImageSeriesRel.image_series_id == ImageSeries.id
+                    ).filter(
+                        ImageSeriesRel.image_id == url_id
+                    ).first()
+                    if image_series:
+                        image_series_category = db_session.query(ImageSeriesCategory).join(
+                            ImageSeriesCategoryRel, ImageSeriesCategoryRel.category_id == ImageSeriesCategory.id
+                        ).filter(
+                            ImageSeriesCategoryRel.series_id == image_series.id
+                        ).first()
+                        if image_series_category:
+                            nav_list.append([image_series_category.name, 2, str(image_series_category.id)])
+                        nav_list.append([image_series.name, 1, str(image_series.id)])
+                elif url_type == 'image_list':
+                    image_series = db_session.query(ImageSeries).get(url_id)
+                    if image_series:
+                        image_series_category = db_session.query(ImageSeriesCategory).join(
+                            ImageSeriesCategoryRel, ImageSeriesCategoryRel.category_id == ImageSeriesCategory.id
+                        ).filter(
+                            ImageSeriesCategoryRel.series_id == image_series.id
+                        ).first()
+                        if image_series_category:
+                            nav_list.append([image_series_category.name, 2, str(image_series_category.id)])
+                        nav_list.append([image_series.name, 1, str(image_series.id)])
+                elif url_type == 'image_series_list':
+                    image_series_category = db_session.query(ImageSeriesCategory).get(url_id)
+                    if image_series_category:
+                        nav_list.append([image_series_category.name, 2, str(image_series_category.id)])
+            return nav_list
+        else:
+            return nav_list
 
 if __name__ == '__main__':
     pass
