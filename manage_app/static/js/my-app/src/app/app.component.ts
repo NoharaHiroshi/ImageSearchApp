3750f41f02f21535c221b2cd7578bdc14ffaf19b
Lands <380788433@qq.com>
2018-05-21 18:50:30 +0800

    用户登录

--- manage_app/static/js/my-app/src/app/app.component.ts
+++ manage_app/static/js/my-app/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { AppConfig } from './config/app_config';
 
+import { HeaderService } from './common/header/header.service';
+import { ListBaseComponent } from './common/base.component';
+
 declare var $: any;
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app_index.html',
 })
-export class AppComponent {
-	constructor(public config: AppConfig) {}
+export class AppComponent extends ListBaseComponent implements OnInit {
+	constructor(public config: AppConfig, public service: HeaderService) {
+		super();
+	}
+	
+	ngOnInit(): void {
+		this.service.getCurUser().then(user => {
+			this.config.user = user;
+        });
+	}
 }
