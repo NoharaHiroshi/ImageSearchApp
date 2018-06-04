04b998d2d64cc7e1565c7055b87cd694777f14d9
Lands <380788433@qq.com>
2018-06-04 18:15:09 +0800

    图片背景处理，解决图片背景为单色的图片抠图问题

--- script/handler_image.py
+++ script/handler_image.py
@@ -6,6 +6,7 @@
 
 import os
 import re
+import traceback
 import contextlib
 import ImageFilter
 import ImageEnhance
@@ -29,6 +30,54 @@ def open_image(file_name):
         print e
     finally:
         pass
+
+
+def get_area_image(img_name, alw):
+    try:
+        with open_image(img_name) as image:
+            new_img = image.im
+            width, height = new_img.size
+            r, g, b = new_img.split()
+            alpha = b
+            main_alpha = alpha.getdata()
+            main_color = main_alpha[0]
+            # 扫描主区域
+            for h in range(height):
+                row_info = list()
+                row_info.append(0)
+                # 是否进入素材
+                is_area = False
+                for w in range(width):
+                    pixel = alpha.getpixel((w, h))
+                    # 进入素材
+                    if pixel < main_color - alw:
+                        if not is_area:
+                            row_info.append(w)
+                            is_area = True
+                    # 离开素材
+                    if is_area:
+                        if pixel >= main_color - alw:
+                            row_info.append(w)
+                            is_area = False
+                row_info.append(width)
+                # 是否进入素材
+                is_a = False
+                for row_area_i in range(len(row_info) - 1):
+                    for row_area in range(row_info[row_area_i], row_info[row_area_i+1]):
+                        if not is_a:
+                            alpha.putpixel((row_area, h), 0)
+                        if is_a:
+                            alpha.putpixel((row_area, h), 225)
+                    is_a = not is_a
+            # 重新分解通道
+            r, g, b = new_img.split()
+            a_img = Image.merge('RGBA', (r, g, b, alpha))
+            name = img_name.split('.')[0] + '_new'
+            t = 'png'
+            new_name = '.'.join([name, t])
+            a_img.save(new_name)
+    except Exception as e:
+        print traceback.format_exc(e)
 
 
 def get_row_main_image(img, value):
@@ -131,6 +180,6 @@ def convert_trans_pic(img):
         print e
 
 if __name__ == '__main__':
-    convert_trans_pic('flower.jpg')
+    get_area_image('phone.jpg', 10)
 
 
