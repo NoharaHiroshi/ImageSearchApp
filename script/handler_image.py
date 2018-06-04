fa33630984059bb76715bc058c58586cd647ec76
Lands <380788433@qq.com>
2018-06-04 18:44:35 +0800

    不使用原来旧的方法

--- script/handler_image.py
+++ script/handler_image.py
@@ -68,7 +68,7 @@ def get_area_image(img_name, alw):
                             alpha.putpixel((row_area, h), 0)
                         if is_a:
                             pixel = alpha.getpixel((row_area, h))
-                            alpha.putpixel((row_area, h), 255-int(pixel*0.4))
+                            alpha.putpixel((row_area, h), 255-int(pixel*0.3))
                     is_a = not is_a
             # 重新分解通道
             r, g, b = new_img.split()
@@ -79,108 +79,9 @@ def get_area_image(img_name, alw):
             a_img.save(new_name)
     except Exception as e:
         print traceback.format_exc(e)
-
-
-def get_row_main_image(img, value):
-    try:
-        width, height = img.size
-        r, g, b = img.split()
-        alpha = b
-        main_alpha = alpha.getdata()
-        first_p = main_alpha[0]
-        for h in range(height):
-            start = 0
-            end = 0
-            for w in range(width):
-                pixel = alpha.getpixel((w, h))
-                if pixel < first_p - value:
-                    start = w
-                    break
-            for _w in range(width)[::-1]:
-                pixel = alpha.getpixel((_w, h))
-                if pixel < first_p - value:
-                    end = _w
-                    break
-            pixel_start_list = range(0, start)
-            pixel_end_list = range(end, width)
-            pixel_list = range(start, end)
-            for p in pixel_start_list:
-                alpha.putpixel((p, h), 0)
-            for p in pixel_end_list:
-                alpha.putpixel((p, h), 0)
-            for p in pixel_list:
-                alpha.putpixel((p, h), 255)
-        return alpha
-    except Exception as e:
-        print e
-
-
-# 获取图片主体(纵)
-def get_col_main_image(img, value):
-    try:
-        width, height = img.size
-        r, g, b = img.split()
-        alpha = b
-        main_alpha = alpha.getdata()
-        first_p = main_alpha[0]
-        for w in range(width):
-            start = 0
-            end = 0
-            for h in range(height):
-                pixel = alpha.getpixel((w, h))
-                if pixel < first_p - value:
-                    start = h
-                    break
-            for _h in range(height)[::-1]:
-                pixel = alpha.getpixel((w, _h))
-                if pixel < first_p - value:
-                    end = _h
-                    break
-            pixel_start_list = range(0, start)
-            pixel_end_list = range(end, height)
-            pixel_list = range(start, end)
-            for p in pixel_start_list:
-                alpha.putpixel((w, p), 0)
-            for p in pixel_end_list:
-                alpha.putpixel((w, p), 0)
-            for p in pixel_list:
-                alpha.putpixel((w, p), 255)
-        return alpha
-    except Exception as e:
-        print e
-
-
-def get_main_image(img, value):
-    row_alpha = get_row_main_image(img, value)
-    col_alpha = get_col_main_image(img, value)
-    width, height = img.size
-    for h in range(height):
-        for w in range(width):
-            row_pixel = row_alpha.getpixel((w, h))
-            col_pixel = col_alpha.getpixel((w, h))
-            if row_pixel != col_pixel:
-                col_alpha.putpixel((w, h), 0)
-    alpha = col_alpha
-    return alpha
-
-
-# 测试简单白底图片转换为透明
-def convert_trans_pic(img):
-    try:
-        with open_image(img) as image:
-            new_img = image.im
-            if new_img.mode == 'RGB':
-                r, g, b = new_img.split()
-                a = get_main_image(new_img, 20)
-                a_img = Image.merge('RGBA', (r, g, b, a))
-                a_img.show()
-                # a_img.save('test.png')
-            else:
-                print 'not RGB mode'
-    except Exception as e:
-        print e
+        
 
 if __name__ == '__main__':
-    get_area_image('test_1.jpg', 10)
+    get_area_image('test_7.jpg', 15)
 
 
