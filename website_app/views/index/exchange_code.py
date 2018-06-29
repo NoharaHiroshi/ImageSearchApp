b3a59f6e029e6762b08dae03d853864c48b775e2
Lands <380788433@qq.com>
2018-06-29 10:19:26 +0800

    优惠券兑换规则

--- website_app/views/index/exchange_code.py
+++ website_app/views/index/exchange_code.py
@@ -31,16 +31,49 @@ def exchange_code():
             if code_query:
                 # 查询兑换码状态
                 if code_query.status == DiscountCode.STATUS_NEW:
+                    # 券码对应的权益
                     discount = db_session.query(Discount).get(code_query.discount_id)
                     if discount:
-                        customer_discount = CustomerDiscount()
-                        customer_discount.customer_id = current_user.id
-                        customer_discount.discount_id = discount.id
-                        customer_discount.effect_days = discount.effect_days
-                        now = datetime.datetime.now()
-                        customer_discount.effect_start = now
-                        customer_discount.effect_end = now + datetime.timedelta(int(discount.effect_days))
-                        db_session.add(customer_discount)
+                        # 查询当前用户是否已有优惠权益，如果有则续期
+                        customer_discount = db_session.query(CustomerDiscount).filter(
+                            CustomerDiscount.customer_id == current_user.id
+                        ).first()
+                        if customer_discount:
+                            # 权益相同，时间延长
+                            if customer_discount.discount_id == discount.id:
+                                customer_discount.effect_end = customer_discount.effect_end + \
+                                                               datetime.timedelta(int(discount.effect_days))
+                            # 暂时处理为更新新一级权益，只升不降
+                            else:
+                                cus_discount_obj = db_session.query(Discount).get(customer_discount.discount_id)
+                                if cus_discount_obj:
+                                    # 会员当前权益等级大于兑换券权益，拒绝录入
+                                    if cus_discount_obj.level > discount:
+                                        result.update({
+                                            'response': 'fail',
+                                            'info': u'您当前的会员权益高于兑换券权益，无法兑换'
+                                        })
+                                        return jsonify(result)
+                                    else:
+                                        # 升级
+                                        customer_discount.discount_id = discount.id
+                                        customer_discount.effect_end = customer_discount.effect_end + \
+                                                                       datetime.timedelta(int(discount.effect_days))
+                                else:
+                                    result.update({
+                                        'response': 'fail',
+                                        'info': u'当前会员权益不存在，请联系管理员'
+                                    })
+                                    return jsonify(result)
+                        else:
+                            customer_discount = CustomerDiscount()
+                            customer_discount.customer_id = current_user.id
+                            customer_discount.discount_id = discount.id
+                            customer_discount.effect_days = discount.effect_days
+                            now = datetime.datetime.now()
+                            customer_discount.effect_start = now
+                            customer_discount.effect_end = now + datetime.timedelta(int(discount.effect_days))
+                            db_session.add(customer_discount)
                         # 更新优惠券状态
                         code_query.status = DiscountCode.STATUS_USED
                         code_query.customer_id = current_user.id
@@ -49,7 +82,7 @@ def exchange_code():
                     else:
                         result.update({
                             'response': 'fail',
-                            'info': u'当前会员权益不存在，请联系管理员'
+                            'info': u'兑换券会员权益不存在，请联系管理员'
                         })
                 else:
                     result.update({
