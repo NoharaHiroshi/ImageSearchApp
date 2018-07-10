d45163d38de76cb08f3bd37a4f95d6cccfce087a
Lands <380788433@qq.com>
2018-07-10 18:47:15 +0800

    会员评论模型

---
+++ website_app/static/js/my-index/src/app/model/comment.ts
@@ -0,0 +1,10 @@
+export class Comment {
+	id: string;
+	customer_id: string;
+	customer_name: string;
+	content: string;
+	comment_id: string;
+	type: number = 0;
+	created_date: string;
+	modified_date: string;
+}
