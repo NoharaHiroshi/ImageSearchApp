605f269b7ecb262e44f9eaf676cb555b4765957a
Lands <380788433@qq.com>
2018-07-26 11:54:16 +0800

    获取素材脚本

--- script/get_image_scripts/get_3png_image.py
+++ script/get_image_scripts/get_3png_image.py
@@ -8,16 +8,19 @@ import requests
 import time
 import traceback
 import lxml
+import json
+import os
+import random
 from model.script.script_3png_image import PIC3Image
 from model.session import get_session
 from urllib import quote
 from bs4 import BeautifulSoup as bs
 
-query_keyword_url = r'http://3png.com/list.html?k='
+query_keyword_url = r'http://3png.com/list.html'
 
 
 # 重写GET请求方法
-def get_requests(url, data=None, headers=None, timeout=10, times=1, retry=5):
+def get_requests(url, data=None, headers=None, timeout=10, times=1, retry=3):
     try:
         response = requests.get(url, data=data, headers=headers, timeout=timeout)
         if response and response.status_code == 200:
@@ -40,7 +43,7 @@ def get_requests(url, data=None, headers=None, timeout=10, times=1, retry=5):
 
 
 # 重写POST请求方法
-def post_requests(url, data, headers=None, timeout=10, times=1, retry=5):
+def post_requests(url, data, headers=None, timeout=10, times=1, retry=3):
     try:
         response = requests.post(url, data=data, headers=headers, timeout=timeout)
         if response and response.status_code == 200:
@@ -62,7 +65,7 @@ def post_requests(url, data, headers=None, timeout=10, times=1, retry=5):
             return None
 
 
-def query_keyword(keyword):
+def get_keyword(keyword):
     headers = {
         'Accept': 'text/html, application/xhtml+xml, application/xml; q=0.9, image/webp, image/apng, */*; q = 0.8',
         'Accept-Encoding': 'gzip, deflate',
@@ -79,15 +82,47 @@ def query_keyword(keyword):
                       '(KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
     }
     try:
+        list_url = 'http://3png.com/list.html'
         url = query_keyword_url
-        base_url = 'http://3png.com'
-        url = '%s%s' % (url, quote(keyword))
+        url = '%s?k=%s' % (url, quote(keyword))
         response = get_requests(url, headers=headers)
+        url = response.url
+        key_value = url.split('/')[-1].split('.')[0].split('-')[-1]
+        new_keyword_url = '%s?k=%s' % (list_url, key_value)
+        soup = bs(response.text, 'lxml')
+        page_list = soup.select('.page > ul > a > li')
+        max_page = int(page_list[-2].get_text())
+        return new_keyword_url, max_page
+    except Exception as e:
+        print traceback.format_exc(e)
+
+
+def query_keyword(keyword, url=None, page=1, max_page=None):
+    headers = {
+        'Accept': 'text/html, application/xhtml+xml, application/xml; q=0.9, image/webp, image/apng, */*; q = 0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN, zh; q = 0.9',
+        'Connection': 'keep-alive',
+        'Cookie': 'UM_distinctid=1639018ae2914a-03bd3da834d279-3e3d5307-1aeaa0-1639018ae2aa4c; '
+                  'PHPSESSID=ni5ael6fobl8osg14pa2eckbl6; '
+                  'CNZZDATA1261035286=1685858532-1527130688-%7C1532508648; '
+                  'Hm_lvt_7c98f23e75ca58af5a589dcae3391616=1532510012; '
+                  'Hm_lpvt_7c98f23e75ca58af5a589dcae3391616=1532510130',
+        'Host': '3png.com',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
+    }
+    try:
+        if not url or max_page:
+            url, max_page = get_keyword(keyword)
+        page_url = '%s&p=%s' % (url, str(page))
+        print u'------------ 当前获取%s页， 共%s页 ---------------' % (page, max_page)
+        base_url = 'http://3png.com'
+        response = get_requests(page_url, headers=headers)
         soup = bs(response.text, 'lxml')
         img_show_list = soup.select('.img-show > a')
         title_list = soup.select('.pic-title > h4')
-        page_list = soup.select('.page > ul > a > li')
-        max_page = page_list[-2].get_text()
         if img_show_list:
             with get_session() as db_session:
                 for i, img_show in enumerate(img_show_list):
@@ -105,8 +140,73 @@ def query_keyword(keyword):
                         img.pic_id = img_id
                         db_session.add(img)
                 db_session.commit()
+                if page < max_page:
+                    time.sleep(5)
+                    page += 1
+                    query_keyword(keyword, url, page, max_page)
     except Exception as e:
         print traceback.format_exc(e)
 
+
+def download_image(keyword):
+    download_url = 'http://3png.com/beha-down'
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN, zh; q=0.9',
+        'Connection': 'keep-alive',
+        'Content-Length': '11',
+        'Cookie': 'UM_distinctid=1639018ae2914a-03bd3da834d279-3e3d5307-1aeaa0-1639018ae2aa4c; '
+                  'PHPSESSID=ni5ael6fobl8osg14pa2eckbl6; '
+                  'Hm_lvt_7c98f23e75ca58af5a589dcae3391616=1532510012; '
+                  'CNZZDATA1261035286=1685858532-1527130688-%7C1532574174; '
+                  'Hm_lpvt_7c98f23e75ca58af5a589dcae3391616=1532576573',
+        'Host': '3png.com',
+        'Origin': 'http://3png.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    data = {
+        'id': 0
+    }
+    with get_session() as db_session:
+        all_image = db_session.query(PIC3Image).filter(
+            PIC3Image.key_word == keyword,
+            PIC3Image.status == PIC3Image.STATUS_DEFAULT
+        ).all()
+        all_image_count = len(all_image)
+        count = 1
+        base_url = os.path.dirname(__file__)
+        file_url = os.path.join(base_url, 'another_object')
+        file_path = os.path.join(file_url, keyword).replace('\\', '/')
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        for image in all_image:
+            time.sleep(random.randint(3, 10))
+            img_title = image.pic_name
+            img_title = '.'.join([img_title, 'png'])
+            img_name = os.path.join(file_path, img_title).replace('\\', '/')
+            print u'----------------- 正在获取%s 剩余%s----------------' % (img_title, all_image_count - count)
+            if os.path.exists(img_name):
+                img_title = image.pic_name + str(int(time.time()))
+                img_title = '.'.join([img_title, 'png'])
+                img_name = os.path.join(file_path, img_title).replace('\\', '/')
+            pic_id = image.pic_id
+            data.update({
+                'id': pic_id
+            })
+            img_response = post_requests(download_url, data=data, timeout=50, headers=headers)
+            response_result = json.loads(img_response.content)
+            print img_response.content
+            download_image_url = response_result.get('url', None)
+            if download_image_url:
+                download_response = get_requests(url=download_image_url)
+                with open(img_name, 'wb') as f:
+                    f.write(download_response.content)
+                image.status = PIC3Image.STATUS_USED
+                count += 1
+                db_session.commit()
+
 if __name__ == '__main__':
-    query_keyword('花')
+    download_image(u'花')
